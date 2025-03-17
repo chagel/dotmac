83b1b4bd55f3bf0050072a43d71bdbcdd17dc7f3
mgc <chagel@gmail.com>
2025-03-17 16:45:09 -0700

    Feat: Set up GOPATH environment variable in user.fish

--- configs/fish/user.fish
+++ configs/fish/user.fish
@@ -3,5 +3,7 @@ fish_add_path $HOME/.local/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/Caskroom/miniconda/base/bin
 
+set -gx GOPATH $HOME/Develop/go
+
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
 set -gx PATH $PATH $HOME/.lmstudio/bin
