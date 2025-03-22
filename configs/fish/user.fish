ca0e5a805ccaba46b732803949042c65f506519d
mgc <chagel@gmail.com>
2025-03-21 23:29:40 -0700

    Fix: move system specific configs

--- configs/fish/user.fish
+++ configs/fish/user.fish
@@ -3,9 +3,13 @@ fish_add_path $HOME/.local/bin
 fish_add_path /opt/homebrew/bin
 fish_add_path /opt/homebrew/Caskroom/miniconda/base/bin
 
-set -gx GOPATH $HOME/Develop/go
-
 source ~/.orbstack/shell/init2.fish 2>/dev/null || :
+
+set -gx GOPATH $HOME/Develop/go
 set -gx PATH $PATH $HOME/.lmstudio/bin
 set -gx OPENAI_API_KEY (pass show keys/openai_api_key)
 set -gx ANTHROPIC_API_KEY (pass show keys/anthropic_api_key)
+
+abbr install brew install
+abbr uninstall brew remove
+abbr upgrade-system brew upgrade
